876bdf301f7388a417d482f10c6dc3f93cc6ceed
Abhishek Nimbalkar <65445090+absnimbalkar@users.noreply.github.com>
2022-04-02 08:57:59 +0530

    Add files via upload

---
+++ scatter plots.py
@@ -0,0 +1,15 @@
+#  Python program to draw scatter plots
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+iris = pd.read_csv("D:\PHP\iris.csv")
+iris = iris.drop('Id',axis=1)
+le = preprocessing.LabelEncoder()
+iris.Species = le.fit_transform(iris.Species)
+x = iris.iloc[:, :-1].values
+y = iris.iloc[:, 4].values
+plt.scatter(x[:,0], x[:, 3], c=y, cmap ='flag')
+plt.xlabel('Sepal Length cm')
+plt.ylabel('Petal Width cm')
+plt.show()
